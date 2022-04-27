af5cb9840f225f9a9492280754f64c41b4c368ad
Muriel Damazio <murieldps@gmail.com>
2022-04-27 14:48:56 -0300

    feat: Posts page done

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,44 @@
+import Head from "next/head";
+import styles from "./styles.module.scss";
+
 export default function Posts() {
    return (
-
+      <>
+         <Head>
+            <title>Blog - Posts</title>
+         </Head>
+         <main className={styles.postsContainer}>
+            <div className={styles.postsList}>
+               <a href="">
+                  <strong>UI Interactions of the week</strong>
+                  <div className={styles.postsInfos}>
+                     <time>12 Feb 2019</time>
+                     |
+                     <span> React JS and Modals</span>
+                  </div>
+                  <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.</p>
+               </a>     
+               <a href="">
+                  <strong>UI Interactions of the week</strong>
+                  <div className={styles.postsInfos}>
+                     <time>12 Feb 2019</time>
+                     |
+                     <span> React JS and Modals</span>
+                  </div>
+                  <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.</p>
+               </a>     
+               <a href="">
+                  <strong>UI Interactions of the week</strong>
+                  <div className={styles.postsInfos}>
+                     <time>12 Feb 2019</time>
+                     |
+                     <span> React JS and Modals</span>
+                  </div>
+                  <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.</p>
+               </a>     
+            </div>
+         
+         </main>
+      </>
    )
 }
