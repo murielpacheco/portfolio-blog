ea1a75664918a6bc1667358364fcc05ad8583ec8
Muriel Damazio <murieldps@gmail.com>
2022-04-30 15:24:14 -0300

    feat: changing headers li order & fixing overflow bug

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,16 +6,16 @@ export function Header() {
       <header className={styles.header}>
          <ul className={styles.headerNav}>
             <Link href="/" passHref>
-               <li className={styles.headerItem}>Home</li>
-            </Link>
-            <Link href="/projects" passHref>
-               <li className={styles.headerItem}>Projects</li>
+               <li>Home</li>
             </Link>
             <Link href="/posts" passHref>
-               <li className={styles.headerItem}>Blog</li>
+               <li>Blog</li>
+            </Link>
+            <Link href="/projects" passHref>
+               <li>Projects</li>
             </Link>
             <Link href="https://github.com/murielpacheco" passHref>
-               <li className={styles.headerItem}>Github</li>
+               <li>Github</li>
             </Link>
          </ul>
       </header>
